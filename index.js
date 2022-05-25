3d485eab82e5edd71201288e1c0c32b9dfb080d0
padln <81055477+padln@users.noreply.github.com>
2022-05-24 23:25:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=3;
-Typer.file="padln.txt"; // add your own name here
+Typer.file="palomar.txt"; // add your own name here
 Typer.init();
  
 var timer = setInterval("t();", 30);
