60617cab85dc38feed256bae3710e86860d996ba
padln <81055477+padln@users.noreply.github.com>
2022-05-24 23:19:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=3;
-Typer.file="[your-name].txt"; // add your own name here
+Typer.file="padln.txt"; // add your own name here
 Typer.init();
  
 var timer = setInterval("t();", 30);
